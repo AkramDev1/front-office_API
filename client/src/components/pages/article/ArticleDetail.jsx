c8dd259b613214d3bd024cae0a83f7a2cbf7893c
AkramDev1 <akramessaidi98@gmail.com>
2021-09-07 17:47:25 +0200

    afficher les questions et les reponses

--- client/src/components/pages/article/ArticleDetail.jsx
+++ client/src/components/pages/article/ArticleDetail.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import usin from "./images/USIN.PNG"
 import Navbar from '../../Navbar';
 import Footerbar from "../../footerbar/Footerbar"
 export class ArticleDetail extends Component {
@@ -10,7 +9,9 @@ export class ArticleDetail extends Component {
         this.state = {
          article:{
 
-         }
+         }, 
+         questions:[],
+         reponses:[]
         }
       }
       componentDidMount(){
@@ -20,22 +21,62 @@ export class ArticleDetail extends Component {
                 this.setState(
                     {article: response.data}
                 )
-            })
+            });
+
+        axios.get(`http://localhost:4001/question/${this.props.match.params.id}`, { })
+            .then((response) => {            
+                console.log("response.data", response.data)
+                //new axios for response:
+                response.data?.map(e=>{
+                    console.log("e", e);
+                     axios.get(`http://localhost:4001/reponse/${e.id_user}/${e.id_quest}`, { })
+                .then((res) => {            
+                    this.setState(
+                        {reponses: res.data}
+                    )
+                       console.log("res", res.data);
+                       this.setState(
+                        {questions:[...this.state.questions,[e, res.data]]}
+                    )
+                });
+                })
+                
+            });
       }
         
 
     render() { 
+        const URL_IMAGES = 'http://localhost:4001/uploads'
+        console.log("question", this.state.questions);
         return (
             <>
             <Navbar />
             <article style={{margin:'15px'}}>  
-                <img src={usin} alt="usin" style={{width:'600px'}}/>
+                <img src={`${URL_IMAGES+'/'+this.state.article.image}`} alt="usin" style={{width:'600px'}}/>
                 <h3 style={{display:"flex"}}>
                     {this.state.article.title} <h5 className="time"> {this.state.article.creat_at}</h5>
                 </h3>
                 <p style={{width : '713px'}}> 
                     {this.state.article.description}
                 </p>
+            </article>
+
+            <article>
+            <div>POSER QUESTION</div>
+                <input type="text"/>
+                <div className="showQuest">
+                    {this.state.questions?.map(element => (
+                        <div>
+                            {element[0].question}
+                            <div>
+                            {element[1]?.map(el =>{
+                                return <h3>{el.reponse}</h3>;
+                            })}
+                        </div>
+                        </div>
+                    ))}
+                </div>
+
             </article>
 
             <Footerbar />
